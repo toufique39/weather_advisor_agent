0b1929134ab4fd5782982424710da9ee0cfc9fb5
Toufique Ahmed <tasniaislam112233@gmail.com>
2025-11-29 19:28:48 +0600

    Update run_weather_agent.py

--- runner/run_weather_agent.py
+++ runner/run_weather_agent.py
@@ -1,1 +1,17 @@
+from agents.weather_agent import process_weather
+
+def run_agent():
+    city = input("Enter a city name: ")
+
+    result = process_weather(city)
+
+    print("\n========== WEATHER REPORT ==========")
+    print(result["summary"])
+    print("\n============== ALERT ===============")
+    print(result["alert"])
+    print("====================================\n")
+
+
+if __name__ == "__main__":
+    run_agent()
 
